348dfd157827c2e768786f3ef4b1abd52f2ab3f2
MeLiger <germantorres@mercadolibre.com>
2022-09-23 13:14:35 -0300

    gobasic1

---
+++ clima.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		temperatura float64 = 17.5
+		humedad     int     = 18
+		presion     int     = 25
+	)
+	fmt.Print(temperatura, humedad, presion)
+
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nombre = "Germán"
+
+	var direccion = "Moreto 571"
+	fmt.Print(nombre, "\n", direccion)
+
+}
