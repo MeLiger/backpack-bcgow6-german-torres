af885d1793887da29664beb2cf107d0d2516f8f7
MeLiger <germantorres@mercadolibre.com>
2022-09-25 15:44:28 -0300

    ejer2

---
+++ GO operators/ejercicio2-1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	w := [7]string{"C", "e", "l", "u", "l", "a", "r"}
+
+	fmt.Println(len(w))
+	fmt.Println(w[0], w[1], w[2], w[3], w[4], w[5], w[6])
+
+}
