0ccae0fb5816feb047dd0673b4142c3673097214
MeLiger <germantorres@mercadolibre.com>
2022-09-27 13:40:24 -0300

    pack

---
+++ GO packages/ejercicio5-1.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type Product struct {
+	ID       string
+	Price    int
+	Quantity int
+}
+
+func (p Product) details() string {
+	return fmt.Sprintf("ID: %s\nPrice: %d\nQuantity: %d\n\n", p.ID, p.Price, p.Quantity)
+}
+
+func main() {
+	product1 := Product{
+		ID:       "KT-303",
+		Price:    350,
+		Quantity: 15,
+	}
+	product2 := Product{
+		ID:       "T1-112",
+		Price:    500,
+		Quantity: 92,
+	}
+
+	d1 := []byte("hello")
+	err := os.WriteFile("/tmp/dat1", d1, 0644)
+
+	f, err := os.OpenFile("products.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err := f.Write([]byte(product1.details())); err != nil {
+		panic(err)
+	}
+	if _, err := f.Write([]byte(product2.details())); err != nil {
+		panic(err)
+	}
+
+}
