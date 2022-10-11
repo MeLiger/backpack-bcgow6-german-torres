a01223b67dbf92f572ab339274e5b728ae435fc4
MeLiger <germantorres@mercadolibre.com>
2022-10-11 01:28:56 -0300

    pkgweb2

--- Arquitectura.GoWeb/pkg/web/response.go
+++ Arquitectura.GoWeb/pkg/web/response.go
@@ -1,28 +1,18 @@
 package web
 
-import  (
- 		"encoding/json"
-		"strconv"
+import (
+	"strconv"
 )
 
-{
-	"code": 401,
-	"error": "Token inválido"
-}
-
-{
-	"code": 200,
-	"data": {...}
-}
-
 type Response struct {
-	Code string `json:"code"`
-	Data interface{} `json:"data,omitempty"`
-	Error string 		`json:"error,omitempty"`
+	Code  string      `json:"code"`
+	Data  interface{} `json:"data,omitempty"`
+	Error string      `json:"error,omitempty"`
 }
 
 func NewResponse(code int, data interface{}, err string) Response {
-	if code < 300{
-		return Response{strconv.FormatInt(int64(code),10), data, ""}
+	if code < 300 {
+		return Response{strconv.FormatInt(int64(code), 10), data, ""}
 	}
+	return Response{strconv.FormatInt(int64(code), 10), nil, err}
 }
